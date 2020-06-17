228581a4f242e07537e08dafd43643c97852a173
cburgos <cburgos@nodecentric.com>
2020-06-16 21:36:18 -0500

    added scheduler now it runs everyday at 10pm

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from datetime import datetime, timezone
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import schedule
+import time
 
 # Find todays date and local hour
 today = datetime.today().strftime('%Y-%m-%d')
@@ -22,17 +24,23 @@ uClient.close()
 # Parse the html page
 page_soup = soup(page_html, 'html.parser')
 
-
 # Find the last rect and put the data-count value to an array
 number_of_commits = [item['data-count']
                      for item in page_soup.find_all('rect', attrs={'data-date': today})]
 
 
 #
+def commit_check():
+    print('Scheduler running everyday at 10pm')
+    if number_of_commits[0] == '0':
+        pass_fail = "PASS" if number_of_commits[0] > "0" else "FAIL"
+        f = open("commit_log.txt", "a")
+        f.write(
+            f"  {today}             {number_of_commits[0]}              {pass_fail}  \n-------------|---------------------|------------ \n")
+        f.close()
 
-if number_of_commits[0] == '0' and local_hour < 17:
-    pass_fail = "PASS" if number_of_commits[0] > "0" else "FAIL"
-    f = open("commit_log.txt", "a")
-    f.write(
-        f"  {today}             {number_of_commits[0]}              {pass_fail}  \n-------------|---------------------|------------ \n")
-    f.close()
+schedule.every().day.at('10:00').do(commit_check)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
